943d9fc204c2b368febd651e25ba8dba44c0c54a
Pablo Bellido <pablobellido@uma.es>
2021-04-01 14:04:05 +0200

    Script del ejercicio 11 de la relación del tema 2, en el que a partir de unos datos dados se reflexiona sobre si aplicar un ajuste lineal o exponencial y que se comparen sus coeficientes de determinación para ver cual es más correcto

---
+++ Tema 2 - Modelización Estadística/Ejercicio11.R
@@ -0,0 +1,72 @@
+x <- c(1:5)
+y <- c(1,1,2,4,8)
+N <- 5
+
+plot(x,y)
+
+# Apartado A:
+
+# A la vista del gráfico de dispersión, no parece que un ajuste lineal sea apropiado, ya que vemos como al 
+# principio crece muy despacio, pero conforme la x aumenta sus valores, la y crece mucho más rapido, por lo que
+# podemos apreciar del dibujo con estos pocos datos, parece un crecimiento exponencial, por lo que un ajuste de
+# ese tipo parece más acertado:
+
+# Apartado B:
+
+# Ajuste de una función:  a * b^x
+
+A <- matrix(c(N,sum(x),sum(x), sum((x)^2)),nrow = 2)
+B <- matrix(c(sum(log(y)),sum(x * log(y))))
+
+res <- solve(A,B)
+
+cat(sprintf("Los valores que buscamos para resolver el sistema son  a =  %f b = %f\n", res[1],res[2]))
+cat(sprintf("El ajuste quedaría de la siguinete manera y =  %f * %f^x \n", exp(res[1]),exp(res[2])))
+
+# Apartado C:
+
+# Usando Modelo Exponencial:
+
+# (gsub("x",1,0.435275*1.741101^x))  es equivalente a y_est
+# y_est = (damos valores del vector x a la expresión: y=0.435275*1.741101^x)
+
+y_est = c(0.435275*1.741101,0.435275*1.741101^2,0.435275*1.741101^3,0.435275*1.741101^4,0.435275*1.741101^5)
+
+VarY <- (sum(y^2)/N) - (sum(y)/N)^2
+VarR <- (sum((y-y_est)^2)/N) - (sum(y-y_est)/N)^2
+R2_Exp <- 1 - (VarR/VarY)
+
+# Usando modelo Lineal:
+
+model <- lm(y~x)
+R2_Lin <-summary(model)$r.squared 
+
+cat(sprintf("El coeficiente de determinación del ajuste exponencial es: %f", R2_Exp))
+cat(sprintf("El coeficiente de determinación del ajuste lineal es: %f", R2_Lin))
+cat(sprintf("Por tanto el mejor ajuste es es el exponencial, ya que explica el  %f por ciento de la varianza de y", (R2_Exp*100)))
+
+# Apartado D:
+
+#Coeficientes del modelo Exponencial.
+exp <- list(exp(res[1]),exp(res[2]))
+# Coeficientes del modelo Lineal:
+lin <- model$coefficients
+
+expfunct <- function(x){
+  0.435275 * 1.741101^x
+}
+
+linfunct <- function(x){
+  -1.9 + 1.7*x
+}
+
+# Para x = 6
+Exp1 <-  expfunct(6)
+Lin1 <- linfunct(6)
+
+# Para x = 10
+Exp2 <-  expfunct(10)
+Lin2 <- linfunct(10)
+
+cat(sprintf("Ajuste Exponencial para los valores de y cuando x=6 y x=10: =  %f , %f\n", Exp1,Exp2))
+cat(sprintf("Ajuste Lineal para los valores de y cuando x=6 y x=10: =  %f , %f\n", Lin1,Lin2))
