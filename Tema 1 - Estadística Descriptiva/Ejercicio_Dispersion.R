4952b2bf3d748520460c01d9dba9c50cb4dae547
Pablo Bellido <pablobellido@uma.es>
2021-04-05 20:11:05 +0200

    El Coeficiente de Variacion de Pearson se expresa ahora en porcentaje

--- Tema 1 - Estadística Descriptiva/Ejercicio_Dispersion.R
+++ Tema 1 - Estadística Descriptiva/Ejercicio_Dispersion.R
@@ -5,5 +5,5 @@
 peso <- c(65,60,65,63,68,70,66,71)
 altura <- c(170,150,168,170,175,171,160,180)
 
-CV_Peso <- (sqrt(mean(peso ^2) - mean(peso)^2) / mean(peso))
-CV_Altura <- (sqrt(mean(altura ^2) - mean(altura)^2) / mean(altura))
+CV_Peso <- (sqrt(mean(peso ^2) - mean(peso)^2) / mean(peso)) * 100
+CV_Altura <- (sqrt(mean(altura ^2) - mean(altura)^2) / mean(altura)) * 100
